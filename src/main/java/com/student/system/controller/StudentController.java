81f6242abc1ea8ba8670a01c3d2dad74d7d51242
07Rochak <rochakshrivastav02@gmail.com>
2026-02-23 23:29:54 +0530

    Create Student Feature

--- src/main/java/com/student/system/controller/StudentController.java
+++ src/main/java/com/student/system/controller/StudentController.java
@@ -2,10 +2,14 @@ package com.student.system.controller;
 
 import com.student.system.dto.StudentDto;
 import com.student.system.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -21,4 +25,25 @@ public class StudentController {
         model.addAttribute("students", students);
         return "students";
     }
+
+    @GetMapping("/students/new")
+    public String newStudent(Model model){
+        StudentDto studentDto = new StudentDto();
+        model.addAttribute("student", studentDto);
+        return "create_student";
+    }
+
+    // Binding Result to ensure that if validation has errors then they can be passed on to frontend
+    @PostMapping("/students")
+    public String createStudent(@Valid @ModelAttribute("student") StudentDto studentDto,
+                                BindingResult result,
+                                Model model){
+        if(result.hasErrors()){
+            model.addAttribute("student", studentDto);
+            return "create_student";
+        }
+        studentService.createStudent(studentDto);
+        return "redirect:/students";
+    }
+
 }
--- src/main/java/com/student/system/dto/StudentDto.java
+++ src/main/java/com/student/system/dto/StudentDto.java
@@ -1,5 +1,7 @@
 package com.student.system.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class StudentDto {
     private long id;
+    @NotEmpty(message = "Student First name should not be empty")
     private String firstName;
+    @NotEmpty(message = "Student Last name should be not empty")
     private String lastName;
+    @NotEmpty(message = "Student Email should not be empty")
+    @Email
     private String email;
 }
